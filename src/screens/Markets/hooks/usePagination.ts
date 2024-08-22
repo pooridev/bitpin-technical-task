ee91e0609e410ea3b1bbd1a5a0b8f6839328057e
Pooria Faramarzian <pooriafaramarzian@gmail.com>
2024-08-22 12:52:17 +0330

    fix(usePagination): immutability issue in paginatedItems

--- src/screens/Markets/hooks/usePagination.ts
+++ src/screens/Markets/hooks/usePagination.ts
@@ -13,12 +13,14 @@ interface PaginationResult<T> {
   goToPage: (page: number) => void
 }
 
+const deepClone = <T extends object>(object: T) => JSON.parse(JSON.stringify(object)) as T
+
 function usePagination<T>({ items, itemsPerPage }: PaginationProps<T>): PaginationResult<T> {
   const [currentPage, setCurrentPage] = useState(1)
 
   const totalPages = Math.ceil(items.length / itemsPerPage)
 
-  const paginatedItems = items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+  const paginatedItems = deepClone(items).slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
 
   const goToPage = (page: number) => {
     setCurrentPage(page)
